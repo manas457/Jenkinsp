bc28b192e3618850a2ec90426a047934f40188a1
manas457 <89373201+manas457@users.noreply.github.com>
2026-06-05 10:47:40 +0530

    Update GoodMorning.java

--- GoodMorning.java
+++ GoodMorning.java
@@ -1,5 +1,7 @@
 public class GoodMorning {
     public static void main(String[] args) {
         System.out.println("Good Morning!");
+        System.out.println("Good Evening!");
+        System.out.println("Good Night!");
     }
 }
